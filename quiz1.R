e6a7d351403655102e13086adc1203132605cf71
r0byGH <roberto.g.contri@gmail.com>
2014-07-10 17:39:37 +0200

    quiz 1 part2

--- quiz1.R
+++ quiz1.R
@@ -24,3 +24,41 @@ q1_downloadxlsx <- function(){
   ## lst = readWorksheet(wb, sheet = 1)  
   lst = readWorksheetFromFile("./data/NGAP.xlsx", sheet = 1, startRow=18, endRow=23, startCol=7, endCol=15)  
 }
+
+q1_downloadxml <- function(){
+  library("XML")
+  fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Frestaurants.xml"
+  download.file(fileURL, destfile="./data/restaurants.xml")
+  doc <- xmlTreeParse("./data/restaurants.xml", useInternal=TRUE)
+
+  ## seems like fileURL doesn't work with https so I downloaded it first but
+  ## can use http instead of https and it works!!! just discard the "s"
+  #   fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Frestaurants.xml"
+  #   fileURL2 <- sub('https', 'http', fileURL)
+  #   doc <- xmlTreeParse(fileURL2, useInternal = TRUE)
+  
+  ## can also use:
+  #library(XML)
+  #library(RCurl)
+  #fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Frestaurants.xml"
+  #xData <- getURL(fileURL)
+  #doc <- xmlParse(xData)
+  
+  #rootNode <- xmlRoot(doc)
+  #xmlName(rootNode)
+  zips <- xpathSApply(rootNode,"//zipcode",xmlValue)
+  a <- zips == "21231"
+  length(zips[a])
+}
+
+q1_jsonexample <- function() {
+  library(jsonlite)
+  jsonData <- fromJSON("https://api.github.com/users/jtleek/repos")
+}
+
+q1_datatable <- function(){
+  fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06pid.csv" 
+  DT <- fread(fileURL)
+}
+
+
